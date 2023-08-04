0bf8d625889f82c68062a6d725921f5a8f52983f
shreyash talole <shreyastalole2001@gmail.com>
2023-08-04 12:05:14 +0530

    Expirary date added

--- abc.php
+++
@@ -1,9 +0,0 @@
-<HTML><HEAD>
-<TITLE>Access Denied</TITLE>
-</HEAD><BODY>
-<H1>Access Denied</H1>
- 
-You don't have permission to access "http&#58;&#47;&#47;www&#46;nseindia&#46;com&#47;api&#47;option&#45;chain&#45;indices&#63;" on this server.<P>
-Reference&#32;&#35;18&#46;67277368&#46;1690431710&#46;2fbc66f2
-</BODY>
-</HTML>
